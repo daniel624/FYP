1c0d108e4d6067e0714ff3e6d4c81c3b85923b0e
Daniel <danielwcw06@gmail.com>
2014-02-05 15:44:40 +0800

    2014-02-05 updated weka source files, added data files

--- weka/Freelist_Test.java
+++ weka/Freelist_Test.java
@@ -1,47 +1,84 @@
 package weka;
 
 import java.io.*;
+import data.*;
 
 public class Freelist_Test {
+	private static int getLongestWord(String field) {
+		int length=0, max=0;
+		
+		for (int i=0; i<field.length(); i++) {
+			if (field.charAt(i)==' ') {
+				if (length > max) {
+					max = length;
+				}
+				length = 0;
+			} else {
+				length++;
+			}
+		}
+		
+		if (length > max) max = length;
+		
+		return max;
+	}
+	
 	public static void main(String[] args) {
-		//String filename = "C:/Users/Lenovo/Documents/notes/FYP/freelist_20140118.txt";
-		String filename = "C:/Users/Lenovo/Documents/workspace/FYP/src/data/freelist_new_part.txt";
+		String filename, outfilename;
+		BufferedReader buf;
+		BufferedWriter writer;
+		
+		// training data
+		//filename = "C:/Users/Lenovo/Documents/workspace/FYP/src/data/freelist_train_data.txt";
+		//outfilename = "C:/Users/Lenovo/Documents/workspace/FYP/src/data/train.arff";
+		
+		// testing data
+		filename = "C:/Users/Lenovo/Documents/workspace/FYP/src/data/freelist_test_data.txt";
+		outfilename = "C:/Users/Lenovo/Documents/workspace/FYP/src/data/test.arff";
+		
 		String input;
 		String split1[] = new String[50];
 		String split2[] = new String[50];
 		String type;
+		int longest=0;
 		
 		try {
-			BufferedReader buf = new BufferedReader(new FileReader(filename));
-			String outfilename = "C:/Users/Lenovo/Desktop/test.arff";
-	    	BufferedWriter writer = new BufferedWriter(new FileWriter(outfilename));
+			buf = new BufferedReader(new FileReader(filename));
+			
+	    	writer = new BufferedWriter(new FileWriter(outfilename));
 	    	writer.write("@relation test\n\n");
 	    	writer.write("@attribute no-of-word numeric\n");
 	    	writer.write("@attribute length numeric\n");
-	    	writer.write("@attribute type {A,T,JC}\n\n");
+	    	writer.write("@attribute initials {0,1}\n");
+	    	writer.write("@attribute type {A,T,JC}\n");
+	    	writer.write("@attribute keyword {0,1}\n");
+	    	writer.write("@attribute preposition {0,1}\n");
+	    	writer.write("@attribute longest-word numeric\n");
+	    	writer.write("@attribute colon-in-title {0,1}\n\n");
 	    	writer.write("@data\n");
 	    	
 			input = buf.readLine();
 			
 			while (input!=null) {
 				split1 = input.split(",");
+				
+				// check type
 				if (split1[1].equals("1")) type = "A";
 				else if (split1[1].equals("2")) type = "T";
 				else if (split1[1].equals("3")) type = "JC";
 				else type = "?";
 				
+				// get longest string within a field
+				longest = getLongestWord(split1[0]);
+				
 				split2 = split1[0].split(" ");
-				writer.write(split2.length + "," + split1[0].length() + "," + type + "\n");
-				
-				/*System.out.println(input);
-				System.out.println("No. of words: " + split.length);
-				System.out.println("Input length: " + input.length());
-				System.out.println("Type: " + type + "\n");*/
-				
+				writer.write(split2.length + "," + split1[0].length() + "," + split1[2] + "," + 
+							type + "," + split1[3] + "," + split1[4] + "," + longest + "," +
+							split1[5] + "\n");
+								
 				input = buf.readLine();
 			}
 
-			writer.write("\n");
 	    	writer.close();
 		} catch (Exception e) {
 			e.printStackTrace();
--- weka/TestWeka.java
+++ weka/TestWeka.java
@@ -1,33 +1,53 @@
 package weka;
 
-import weka.classifiers.evaluation.Evaluation;
-import weka.classifiers.meta.FilteredClassifier;
+/*
+ * update log
+ * 
+ * ===== 2014-01-09 =====
+ * - add weka RandomForest to help on string fields
+ * - use course project data from CSCI 3230 last sem as learning data
+ * 
+ * ===== 2014-01-13 =====
+ * - add length of each field and number of words as testing attributes
+ * 
+ * ===== 2014-01-23 =====
+ * - add 1 more attribute, whether the field contains abbreviated initials in author names 
+ * 
+ * ===== 2014-01-28 =====
+ * - add exact keyword matching
+ * - length of longest word in a field
+ * - preposition mostly appear in title/journal
+ * - ':' in title
+ * 
+ */
+
 import weka.classifiers.trees.*;
 import weka.core.Instances;
-import weka.filters.unsupervised.attribute.Remove;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;
+import weka.classifiers.Evaluation;
+import java.util.*;
 
 public class TestWeka {
     public static void main(String[] args) throws Exception {
     	String filename;
     	BufferedReader buf;
     	
-    	//filename = "C:/Users/Lenovo/Documents/notes/Fall 2013/CSCI 3230/splicing/RA-train.arff";
-    	filename = "C:/Users/Lenovo/Desktop/train.arff";;
+    	//filename = "C:/Users/Lenovo/Documents/workspace/FYP/src/data/RA-train.arff";
+    	filename = "C:/Users/Lenovo/Documents/workspace/FYP/src/data/train.arff";
     	buf = new BufferedReader(new FileReader(filename));
     	Instances train = new Instances(buf);
-    	train.setClassIndex(train.numAttributes()-1);
+    	train.setClassIndex(3);
+    	//train.setClassIndex(train.numAttributes()-1);
     	
-    	//filename = "C:/Users/Lenovo/Documents/notes/Fall 2013/CSCI 3230/splicing/RA-test.arff";
-    	filename = "C:/Users/Lenovo/Desktop/test.arff";
+    	//filename = "C:/Users/Lenovo/Documents/workspace/FYP/src/data/RA-test.arff";
+    	filename = "C:/Users/Lenovo/Documents/workspace/FYP/src/data/test.arff";
     	buf = new BufferedReader(new FileReader(filename));
     	Instances test = new Instances(buf);
-    	test.setClassIndex(test.numAttributes()-1);
+    	test.setClassIndex(3);
+    	//test.setClassIndex(test.numAttributes()-1);
     	
     	buf.close();
 
@@ -35,19 +55,25 @@ public class TestWeka {
     	tree.setNumTrees(10);
     	tree.buildClassifier(train);
     	
+    	// add cross validation
+    	Evaluation eval = new Evaluation(train);
+    	eval.crossValidateModel(tree, train, 10, new Random(1));
+    	eval.evaluateModel(tree, test);
+    	System.out.println(eval.toSummaryString("\nResults\n======\n", false));
+    	System.out.println(eval.confusionMatrix()[0][0] + "\t" + eval.confusionMatrix()[0][1]);
+    	System.out.println(eval.confusionMatrix()[1][0] + "\t" + eval.confusionMatrix()[1][1]);
+    	
     	Instances labeled = new Instances(test);
     	double clsLabel;
     	for (int i=0; i<test.numInstances(); i++) {
     		clsLabel = tree.classifyInstance(test.instance(i));
     		labeled.instance(i).setClassValue(clsLabel);
-    		//System.out.println(clsLabel);
     	}
     	
-    	filename = "C:/Users/Lenovo/Desktop/result.arff";
+    	filename = "C:/Users/Lenovo/Documents/workspace/FYP/src/data/result.arff";
     	BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
     	writer.write(labeled.toString());
     	writer.close();
-    	//System.out.println(labeled.toString());
     }
 }
 
